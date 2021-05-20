db2c7036ca9ba7e43a951a88d930dc791780e5e1
zktanvir <zktanvir2005@gmail.com>
2021-05-20 22:34:41 +0600

    added new fun commands

--- Cogs/fun.py
+++ Cogs/fun.py
@@ -23,9 +23,8 @@ except:
 	import giphypop
 
 
-token = os.environ.get('WIKITOKEN')
-apikey= os.environ.get("TENOR")
-nasa = os.environ.get("NASA")
+TENOR_API_KEY= os.environ.get("TENOR")
+NASA_API_KEY = os.environ.get("NASA")
 
 class Fun(commands.Cog):
 	def __init__(self, bot):
@@ -228,7 +227,7 @@ class Fun(commands.Cog):
 		
 	@commands.command()
 	async def apod(self, ctx):
-		url = f"https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+		url = f"https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}"
 		response = requests.get(url)
 		data = json.loads(response.text)
 		image = data["hdurl"]
@@ -290,5 +289,84 @@ class Fun(commands.Cog):
 		await ctx.send(embed=em)
 		
 		
+    	@commands.command()
+	@commands.cooldown(5, 30, commands.BucketType.user)
+	async def feed(self, ctx, member: discord.Member = None):
+		if member is None:
+			await ctx.send('*feeds air*')
+			return False
+		url = nekos.img('feed')
+		em = embeds.fun_embed(img=url, footer="Powered by nekos.life")
+		await ctx.send(embed=em)
+
+
+	@commands.command(aliases=["cmm"])
+	async def changemymind(self, ctx, *, text=None):
+		if text is None:
+			await ctx.send("Provide some text")
+			return False
+		api = NekoBotAsync()
+		img = (await api.changemymind(text)).message
+		em = embeds.fun_embed(img=img, footer="")
+		await ctx.send(embed=em)
+
+	@commands.command()
+	async def magik(self, ctx, member:discord.Member=None):
+		async with ctx.typing():
+			if member is None:
+				member = ctx.author
+			img = f"https://cdn.discordapp.com/avatars/{member.id}/{member.avatar}.jpg?size=1024"
+			async with aiohttp.ClientSession().get(f"https://nekobot.xyz/api/imagegen?type=magik&image={img}") as resp:
+				data = json.loads(await resp.text())
+			url = data["message"]
+			em = embeds.fun_embed(title="Magik",footer="Powered by nekobot.xyz",img=url)
+		await ctx.send(embed=em)
+
+	@commands.command()
+	async def ship(self, ctx, user1:discord.Member, user2:discord.Member=None):
+		if user2 is None:
+			user2 = ctx.author
+		user1av = f"https://cdn.discordapp.com/avatars/{user1.id}/{user1.avatar}.jpg?size=1024"
+		user2av = f"https://cdn.discordapp.com/avatars/{user2.id}/{user2.avatar}.jpg?size=1024"
+		async with ctx.typing():
+			async with aiohttp.ClientSession().get(f"https://nekobot.xyz/api/imagegen?type=ship&user1={user1av}&user2={user2av}") as resp:
+				data = json.loads(await resp.text())
+			img = data["message"]
+			name1 = user1.name
+			name2 = user2.name
+			len1 = round(len(name1)/2)
+			len2 = round(len(name2)/2)
+			name1 = name1[:len1]
+			name2 = name2[len2:]
+			name = name1 + name2
+			em = embeds.fun_embed(title=f"Shipped {user1.name} & {user2.name}", des=f"We call it {name.title()}", img=img, footer="Powered by nekobot.xyz")
+		await ctx.send(embed=em)
+
+
+	@commands.command()
+	async def deepfry(self, ctx, user:discord.Member=None):
+		if user is None:
+			user = ctx.author
+		async with ctx.typing():
+			async with aiohttp.ClientSession().get(f"https://nekobot.xyz/api/imagegen?type=deepfry&image=https://cdn.discordapp.com/avatars/{user.id}/{user.avatar}.jpg?size=1024") as resp:
+				data = await resp.json()
+			img = data["message"]
+			em = embeds.fun_embed(title="",img=img,footer="Powered by nekobot.xyz")
+		await ctx.send(f"Deepfried {user.name}",embed=em)
+
+
+	@commands.command(aliases=['randomquote','rquote'])
+	async def quote(self, ctx):
+		resp = requests.get("https://type.fit/api/quotes")
+		data = resp.json()
+		one = random.choice(data)
+		author = one["author"]
+		text = one["text"]
+		em = embeds.fun_embed(title="Random Quotes", des=f'{text}\n{author:>40}')
+		await ctx.send(embed=em)
+
+
+
+
 def setup(bot):
 	bot.add_cog(Fun(bot))
